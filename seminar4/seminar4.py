37a8b73c8c608cd6918f8616f9138db54eefaba1
Svetlana Kovalenko <milkshake9273@gmail.com>
2023-04-25 14:42:49 +1000

    24

--- seminar4/seminar4.py
+++ seminar4/seminar4.py
@@ -57,10 +57,101 @@
 # .split()
 
 # my_string = list(input("Введите строку из символов: "))
+# # вводим строку и переводим в список
 # print(my_string)
 # print(my_string[0], end=" ")
+# # печатаем первый элемент
 # for i in range(1, len(my_string)):
+#   # считаем элементы с 1
 #   print(f"{my_string[i]}", end=" ")
 #   count = my_string[:i-1].count(my_string[i])
+#   # делаем срез и считаем, увеличиваем каунт
+#   # сразу без срезов нельзя потому что на момент вхождения
 #   if count > 0:
 #     print(f"_{count}", end=" ")
+
+
+# my_string2 = list(input("Введите строку из символов: "))
+# count_dict = {}
+# for letter in my_string2:
+#   count_dict[letter] = count_dict.get(letter, -1) + 1 
+#   # берем из словаря то что там лежит до
+#   #  если у нас есть значение - вернет, если нет то ноль
+#   #  если уже лежало увеличиваем на 1
+#   print(f"{letter}" if count_dict.get(letter) 
+#         == 0 else f"{letter}_{count_dict.get(letter)-1}", end = " ")
+
+
+
+# Задача №27. Решение в группах
+# Пользователь вводит текст(строка). Словом считается
+# последовательность непробельных символов идущих
+# подряд, слова разделены одним или большим числом
+# пробелов. Определите, сколько различных слов
+# содержится в этом тексте.
+# Input: She sells sea shells on the sea shore The shells
+# that she sells are sea shells I'm sure.So if she sells sea
+# shells on the sea shore I'm sure that the shells are sea
+# shore shells
+# my_text = input("Введите текст: ").split()
+# print (my_text)
+# text_result = set()
+# for word in my_text:
+#   text_result.add(word.lower())
+# print (len(text_result))
+
+
+# **Задача 22:**
+# Даны два неупорядоченных набора целых чисел (может быть, с повторениями). 
+# Выдать без повторений в порядке возрастания все те числа, 
+# которые встречаются в обоих наборах.
+# Пользователь вводит 2 числа. n — кол-во элементов первого множества. 
+# m — кол-во элементов второго множества. Затем пользователь вводит 
+# сами элементы множеств.
+
+# import random
+# n = int(input("введите длину первого ряда чисел: "))
+# m = int(input("введите длину второго ряда чисел: "))
+# first_set = set()
+# both_in_two = set()
+# for i in range(n):
+#     i = random.randint(1, 10)
+#     print(i, end=" ")
+#     first_set.add(i)
+# print()
+# for i in range(m):
+#     i = random.randint(1, 10)
+#     print(i, end=" ")
+#     if i in first_set:
+#       both_in_two.add(i) 
+# print()       
+# print(sorted(both_in_two)) 
+
+
+# Задача 24: В фермерском хозяйстве в Карелии выращивают чернику. 
+# Она растёт на круглой грядке, причём кусты высажены только по окружности. 
+# Таким образом, у каждого куста есть ровно два соседних. Всего на грядке растёт N кустов.
+# Эти кусты обладают разной урожайностью, поэтому ко времени 
+# сбора на них выросло различное число ягод — на i-ом кусте выросло ai ягод.
+# В этом фермерском хозяйстве внедрена система автоматического сбора черники. 
+# Эта система состоит из управляющего модуля и нескольких собирающих модулей. 
+# Собирающий модуль за один заход, находясь непосредственно перед некоторым кустом, 
+# собирает ягоды с этого куста и с двух соседних с ним.
+# Напишите программу для нахождения максимального числа ягод, 
+# которое может собрать за один заход собирающий модуль, находясь перед некоторым кустом 
+# заданной во входном файле грядки.
+
+import random
+number_of_plants = int(input("введите количество кустов: "))
+list_of_plants = list()
+for i in range(number_of_plants):
+  i = random.randint(1, 10)
+  print(i, end=" ")
+  list_of_plants.append(i)
+  
+max_to_bring = list()
+for i in range(len(list_of_plants)-1):
+  max_to_bring.append(list_of_plants[i-1]+list_of_plants[i]+list_of_plants[i+1])
+max_to_bring.append(list_of_plants[-2]+list_of_plants[-1]+list_of_plants[0])
+print()
+print(max(max_to_bring))
