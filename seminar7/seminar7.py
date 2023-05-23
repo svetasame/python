b0a3ad90dd9389ba4bec576580071de68b548cfe
Svetlana Kovalenko <milkshake9273@gmail.com>
2023-05-23 20:19:25 +1000

    две задачи

--- seminar7/seminar7.py
+++ seminar7/seminar7.py
@@ -41,16 +41,43 @@ import random
 orbits_1 = [(3, 4), (6, 9), (12, 3), (9, 5), (7, 7), (8, 13)]
 
 
-size = int(input("Введите список планет: "))
-orbits = [(random.randint(1, 10), random.randint(1, 10)) for i in range(size)]
-print(orbits)
-
+# size = int(input("Введите список планет: "))
+# orbits = [(random.randint(1, 10), random.randint(1, 10)) for i in range(size)]
+# print(orbits)
 
 def find_farthest_orbit(list_of_orbits: list) -> tuple:
   dict_ = {round(pi*orbit[0]/2 * orbit[1]/2, 2): orbit for orbit in
            filter(lambda orbit: orbit[0] != orbit[1], list_of_orbits)}
   return dict_
 
+# dict_p = find_farthest_orbit(orbits)
+# print(max(dict_p), dict_p[max(dict_p)])
 
-dict_p = find_farthest_orbit(orbits)
-print(max(dict_p), dict_p[max(dict_p)])
+
+
+# Задача №51. Решение в группах
+# Напишите функцию same_by(characteristic, objects), которая
+# проверяет, все ли объекты имеют одинаковое значение
+# некоторой характеристики, и возвращают True, если это так.
+# Если значение характеристики для разных объектов
+# отличается - то False. Для пустого набора объектов, функция
+# должна возвращать True. Аргумент characteristic - это
+# функция, которая принимает объект и вычисляет его
+# характеристику.
+# Ввод: Вывод:
+# values = [0, 2, 10, 6] same
+# if same_by(lambda x: x % 2, values):
+# print(‘same’)
+# else:
+# print(‘different’)
+
+quant = int(input("Введите количество объектов: "))
+objects = [random.randint(1, 10) for i in range(quant)]
+print(objects)
+
+def same_by(characteristic, values):
+  my_set = set(map(characteristic, values))
+  return len(my_set) < 2
+   
+
+print(same_by(lambda values: values%2, objects))
